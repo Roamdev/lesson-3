0d2e0e745a942acef161227b5e45c1e1223eb419
Roamdev <aroundthefog@gmail.com>
2020-02-25 16:23:19 +0400

    complete(?)

--- src/PersonalForm.js
+++ src/PersonalForm.js
@@ -1,9 +1,32 @@
 import React, {PureComponent} from 'react';
 
 class PersonalForm extends PureComponent {
+
+  handleChangeForm = (event) => {
+    const { value, name } = event.target
+    this.props.onChangeForm(name, value);
+};
   render() {
-    return <div> zalupa </div>
-  }
+    return (
+      <>
+        <div className='personal-form'></div>
+        <input
+          className='personal-form'
+          name="firstName"
+          onChange={this.props.handleChangeForm}
+        />
+        <input
+          className='personal-form'
+          name="lastName"
+          onChange={this.props.handleChangeForm}
+        />
+        <input
+          className='personal-form'
+          name="email"
+          onChange={this.props.handleChangeForm}
+        />
+      </>
+    )}
 }
 
 
